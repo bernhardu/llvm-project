7480fcf15f886fb6bc573c1c818a3fb46c9dbd06
Bernhard Übelacker <bernhardu@mailbox.org>
2025-08-06 15:14:49 +0200

    Workarounds about linking error related to __except_handler4 at i686.
    Avoids error:
         msvcrt.lib(chandler4gs.obj) : error LNK2019: unresolved external symbol __except_handler4_common referenced in function __except_handler4

--- compiler-rt/lib/sanitizer_common/tests/sanitizer_test_main.cpp
+++ compiler-rt/lib/sanitizer_common/tests/sanitizer_test_main.cpp
@@ -21,3 +21,15 @@ int main(int argc, char **argv) {
   __sanitizer::SetCommonFlagsDefaults();
   return RUN_ALL_TESTS();
 }
+
+// Ugly workaround against a link error with clang++ at i686
+// Avoids error:
+//     msvcrt.lib(chandler4gs.obj) : error LNK2019: unresolved external symbol __except_handler4_common referenced in function __except_handler4
+#if defined(_WIN32) && defined(__i386__)
+extern "C" int _except_handler4(void *, void *, void *, void *);
+extern "C" {
+int _except_handler4_common(void *a, void *b, void *c, void *d) {
+  return _except_handler4(a, b, c, d);
+}
+}
+#endif
